520c900c9c5ce4f419f7983ffacfc5c57d415e8d
WillianAlexander <alexfj-789@hotmail.com>
2025-07-07 16:30:43 -0500

    fuera de local

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://192.168.112.131:4200' });
+  app.enableCors({
+    origin: 'http://192.168.112.131:4200',
+    methods: ['POST'],
+  });
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
